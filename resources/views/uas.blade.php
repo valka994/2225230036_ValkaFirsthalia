9612a5d4bd6c09e652669572b81e41a34ad175c7
valka994 <2225230036@untirta.ac.id>
2023-12-12 15:52:07 +0700

    caption

---
+++ resources/views/uas.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Home</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+        <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <h1>Home</h1>
+
+    <header>
+      <nav class="navbar">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
